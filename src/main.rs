a60486df6ebbfc48c43ccebc4c3cf112025ede73
NekokeCore <Core2002@163.com>
2022-06-12 00:39:31 +0800

    添加了在没有管理员权限的情况下运行程序的提示

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate winreg;
+extern crate winapi;
 use winreg::enums::*;
 use winreg::RegKey;
 use std::fs;
@@ -12,7 +13,12 @@ fn main() {
     let file_cat_empty = "C:\\cat_empty.dll";
     let file_cat_full = "C:\\cat_full.dll";
 
-    fs::write(file_cat_empty,cat_ico_empty).unwrap();
+    if fs::write(file_cat_empty,cat_ico_empty).is_err() {
+        println!("请右键 -> 管理员身份运行");
+        message_box("请右键 -> 管理员身份运行");
+        panic!();
+    }
+    
     fs::write(file_cat_full, cat_ico_full).unwrap();
 
     let reg_key = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\{645FF040-5081-101B-9F08-00AA002F954E}\\DefaultIcon";
@@ -50,6 +56,17 @@ fn main() {
     println!("友情提示：假如没反应就打开关闭然后多刷新几次");
     
     io::stdin().read_line(&mut buf).unwrap();
+}
+
+
+fn message_box(msg: &str){
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    use std::ptr::null_mut;
+    use winapi::um::winuser::{MessageBoxW, MB_OK};
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    unsafe { MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK) };
 }
 
 // HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon
