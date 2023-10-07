e4d8ec8486235fd7ef91f944a7b59da582d9db5c
NekokeCore <Core2002@163.com>
2023-10-07 18:59:14 +0800

    将渲染判定的v-if改为v-show

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ invoke("cmd_has_permissions_and_release_files").then(b => {
 </script>
 
 <template>
-  <div v-if="permissionsMsg == '权限正常'" class="row">
+  <div v-show="permissionsMsg == '权限正常'" class="row">
     <div class="container">
       <h1>请选择图标样式 ! </h1>
       <p>Plase select icon !</p>
@@ -23,7 +23,7 @@ invoke("cmd_has_permissions_and_release_files").then(b => {
   </div>
 
   <div class="container">
-    <div v-if="permissionsMsg == '无管理员权限'" class="row">
+    <div v-show="permissionsMsg == '无管理员权限'" class="row">
       <el-col :sm="12" :lg="6">
         <el-result icon="error" title='无管理员权限' sub-title="请右键->以管理员身份运行">
         </el-result>
